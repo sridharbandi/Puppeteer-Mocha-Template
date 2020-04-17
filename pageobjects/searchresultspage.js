329eeb2ede4994dba9fc9ae795dff3573c159f0c
Sridhar Bandi <sridhar.bandi.ece@gmail.com>
2020-04-17 18:46:53 +0200

    Updated locators

--- pageobjects/searchresultspage.js
+++ pageobjects/searchresultspage.js
@@ -2,13 +2,13 @@ export default class SearchResultsPage{
 
     constructor(page) {
         this.page = page;
-        this.LINK_SELENIUM = 'h3 a';
+        this.LINK_SELENIUM = '.rc h3';
     }
 
     async isSeleniumPresent(){
         await this.page.waitForSelector(this.LINK_SELENIUM)
         const links = await this.page.evaluate(() => {
-            return Array.from(document.querySelectorAll('h3 a'))
+            return Array.from(document.querySelectorAll('.rc h3'))
                 .map(a => { return a.textContent })
         })
         return links[0];
